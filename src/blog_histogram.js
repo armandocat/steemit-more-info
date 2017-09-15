7210823a0d9d50226535a26fccbb9b7fcf3647fe
Armando Cat <armandocat@gmx.com>
2017-09-15 15:53:55 +0200

    Fix histogram posts list scroll

--- src/blog_histogram.js
+++ src/blog_histogram.js
@@ -160,6 +160,17 @@
       postsList.append(createPost(name, post));
     });
 
+    // prevent page scroll if mouse is no top of the list
+    postsList.bind('mousewheel DOMMouseScroll', function (e) {
+      var delta = e.wheelDelta || (e.originalEvent && e.originalEvent.wheelDelta) || -e.detail,
+          bottomOverflow = this.scrollTop + $(this).outerHeight() - this.scrollHeight >= 0,
+          topOverflow = this.scrollTop <= 0;
+
+      if ((delta < 0 && bottomOverflow) || (delta > 0 && topOverflow)) {
+          e.preventDefault();
+      }
+    });
+
     container.append(postsContainer);
 
     $('html, body').animate({
