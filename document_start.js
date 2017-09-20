f4a73c3c31edcebf304f4765da422d075ba638d1
Armando Cat <armandocat@gmx.com>
2017-09-20 17:45:03 +0200

    Disable web socket hooks

--- document_start.js
+++ document_start.js
@@ -1,16 +1,16 @@
 
-[
-'vendor/wsHook.js',
-'src/utils/websocket_hook.js'
-].reverse().reduce(function(next, script){
-  return function(){
-    var s = document.createElement('script');
-    s.src = chrome.extension.getURL(script);
-    s.onload = function() {
-      next && next();
-    };
-    s.async = false;
-    document.documentElement.appendChild(s);
-  };
-}, null)();
+// [
+// 'vendor/wsHook.js',
+// 'src/utils/websocket_hook.js'
+// ].reverse().reduce(function(next, script){
+//   return function(){
+//     var s = document.createElement('script');
+//     s.src = chrome.extension.getURL(script);
+//     s.onload = function() {
+//       next && next();
+//     };
+//     s.async = false;
+//     document.documentElement.appendChild(s);
+//   };
+// }, null)();
 
