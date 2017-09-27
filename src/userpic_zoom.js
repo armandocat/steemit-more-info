d1197ca01eedc42e43116eb62d043e335c7a07e2
Armando Cat <armandocat@gmx.com>
2017-09-27 18:22:35 +0200

    Fix Userpic

--- src/userpic_zoom.js
+++ src/userpic_zoom.js
@@ -22,61 +22,96 @@
   };
 
 
-  $('body')
-    .on('mouseenter', '.Userpic', function() {
-      var $this = $(this);
+  var openUserpicZoom = function(thisUserpic, bigImg) {
+    userpic = thisUserpic;
+    setTimeout(function() {
+      if(userpic === thisUserpic && !userpic.find('.smi-userpic-zoom').length){
+
+        var zoom = $('<div class="smi-userpic-zoom">\
+          <img class="smi-userpic-zoom-img" src="' + bigImg + '"></img>\
+        </div>');
+
+        var loading = $(window.SteemMoreInfo.Utils.getLoadingHtml({
+          center: true
+        }));
+
+        zoom.append(loading);          
+
+        userpic.prepend(zoom);
+
+        zoom.find('img').one('load', function() {
+          loading.remove();
+          zoom.removeClass('smi-userpic-zoom-loading');
+          checkSize(zoom);
+        }).each(function() {
+          if(this.complete){
+            loading.remove();
+            zoom.removeClass('smi-userpic-zoom-loading');
+            checkSize(zoom);
+          }else{
+            zoom.addClass('smi-userpic-zoom-loading');
+            checkSize(zoom);
+          }
+        });
+
+      }
+    }, 500);
+  };
+
+
+  var removeUserpicZoom = function(thisUserpic) {
+    thisUserpic.find('.smi-userpic-zoom').remove();
+    if(userpic && userpic.is(thisUserpic)){  
+      userpic = null;
+    }
+  };
+
+
+  $('body').on('mouseenter', '.Userpic', function() {    
+    var $this = $(this);
+    var thisUserpic;
+    var bigImg;
+
+    if($this.is('div')){
+      // Steemit Userpic
+      thisUserpic = $this;
+
+      if($this.closest('.Header__userpic').length){
+        return;
+      }
+
       var backgroundImage = $this.css('background-image');
       if(!backgroundImage || backgroundImage.startsWith('url("/assets/') || backgroundImage.startsWith('url("https://steemit.com/assets/')){
         return;
       }
-      if($this.closest('.Header__userpic').length){
-        return;
-      }
-      userpic = $this;
-      setTimeout(function() {
-        if(userpic === $this && !userpic.find('.smi-userpic-zoom').length){
+      bigImg = backgroundImage.replace(/^url\("https:\/\/steemitimages\.com\/[0-9]*x[0-9]*\/(.*)"\)$/, function(a, b){
+        return 'https://steemitimages.com/512x512/' + b;
+      });
+  
+    }else{
+      // SMI Userpic
+      thisUserpic = $this.parent();
+      
+      var backgroundImage = $this.attr('src');
+      bigImg = backgroundImage.replace(/\?s=[0-9]*/, '?s=512');
 
-          var bigImg = backgroundImage.replace(/^url\("https:\/\/steemitimages\.com\/[0-9]*x[0-9]*\/(.*)"\)$/, function(a, b){
-            return 'https://steemitimages.com/512x512/' + b;
-          });
-          console.log('ìAAAAA', backgroundImage, bigImg);
+    }
+    openUserpicZoom(thisUserpic, bigImg);
+  });
 
-          var zoom = $('<div class="smi-userpic-zoom">\
-            <img class="smi-userpic-zoom-img" src="' + bigImg + '"></img>\
-          </div>');
+  $('body').on('mouseleave', '.Userpic', function() {
+    var $this = $(this);
+    var thisUserpic;
+    if($this.is('div')){
+      // Steemit Userpic
+      thisUserpic = $this;
+    }else{
+      // SMI Userpic
+      thisUserpic = $this.parent();
+    }
+    removeUserpicZoom(thisUserpic);
+  });
 
-          var loading = $(window.SteemMoreInfo.Utils.getLoadingHtml({
-            center: true
-          }));
-
-          zoom.append(loading);          
-
-          userpic.append(zoom);
-
-          zoom.find('img').one('load', function() {
-            loading.remove();
-            zoom.removeClass('smi-userpic-zoom-loading');
-            checkSize(zoom);
-          }).each(function() {
-            if(this.complete){
-              loading.remove();
-              zoom.removeClass('smi-userpic-zoom-loading');
-              checkSize(zoom);
-            }else{
-              zoom.addClass('smi-userpic-zoom-loading');
-              checkSize(zoom);
-            }
-          });
-
-        }
-      }, 500);
-    })
-    .on('mouseleave', '.Userpic', function() {
-      var $this = $(this);
-      if(userpic && $this.is(userpic)){
-        userpic.find('.smi-userpic-zoom').remove();
-        userpic = null;
-      }
-    });
+  
 
 })();
